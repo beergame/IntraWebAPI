fb44f1465f16d088733ff13be20a4ec3afcfdc03
Roland Coulibaly <coulib_y@etna-alternance.net>
2018-05-14 15:16:49 +0200

    try to fix on linux

---
+++ IntraWebApi.Data/Context/ContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace IntraWebApi.Data.Context
+{
+	public class ContextFactory : IDesignTimeDbContextFactory<Context>
+	{
+		public Context CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<Context>();
+			optionsBuilder.UseSqlServer("Server=localhost:1401\\intrawebsql;Database=IntraDB;Trusted_Connection=True;ConnectRetryCount=0");
+			return new Context(optionsBuilder.Options);
+		}
+	}
+}
